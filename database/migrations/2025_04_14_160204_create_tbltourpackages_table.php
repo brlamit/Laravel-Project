c9132472d0c916b8ceba8e1b4b9d7a45af4a4d93
brlamit <baralamit881@gmail.com>
2025-04-14 21:54:08 +0545

    Refactor tbltourpackages migration: replace default id and timestamps with custom fields for package details

--- database/migrations/2025_04_14_160204_create_tbltourpackages_table.php
+++ database/migrations/2025_04_14_160204_create_tbltourpackages_table.php
@@ -12,8 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tbltourpackages', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->integer('PackageId')->unsigned();
+            $table->primary('PackageId');
+            $table->string('PackageName', 200)->nullable();
+            $table->string('PackageType', 150)->nullable();
+            $table->string('PackageLocation', 100)->nullable();
+            $table->integer('PackagePrice')->nullable();
+            $table->string('PackageFetures', 255)->nullable();
+            $table->mediumText('PackageDetails')->nullable();
+            $table->string('PackageImage', 100)->nullable();
+            $table->timestamp('Creationdate')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('UpdationDate')->nullable()->default(DB::raw('NULL ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
